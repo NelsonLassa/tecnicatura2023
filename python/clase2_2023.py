8e03e20ef50e3befedeb5735d33e5b06a915e290
Nelson <nelsonlassa63@gmail.com>
2023-06-05 21:32:47 -0300

    clase 2 - 2023

---
+++ python/clase2_2023.py
@@ -0,0 +1,13 @@
+miVariable = 3
+print(miVariable)
+miVariable = 'Hola a todos los estudiantes de la tecnicatura'
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(id(x))
+# Las literales se escriben x528, la variable y = x272, la variable = x592
+print(id(y))
+print(id(z))
