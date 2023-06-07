876c9c74a7de0425a0ae11319fb7260e383ca148
Lassa <nelsonlassa63@gmail.com>
2023-06-07 20:53:41 -0300

    Leccion 2

--- java/HolaMundo/src/main/java/org/example/HolaMundo.java
+++ java/HolaMundo/src/main/java/org/example/HolaMundo.java
@@ -3,5 +3,17 @@ package org.example;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo desde Java");
+
+        int miVariable = 10;
+        // alt 92 barra invertida
+        System.out.println(miVariable + "\n");
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVariableCadena = "\nBienvenidos";
+        System.out.println(miVariableCadena);
+        //Salto de linea
+        miVariableCadena = "\nSigamos creciendo en programacion ";
+        System.out.println(miVariableCadena);
     }
 }
