7ad833bcceb62ad2df56e14105f93270416ffed0
Lassa <nelsonlassa63@gmail.com>
2023-06-11 18:56:06 -0300

    Maximo y minimo valor de los tipos primitivos enteros y flotantes

--- java/HolaMundo/src/main/java/org/example/HolaMundo.java
+++ java/HolaMundo/src/main/java/org/example/HolaMundo.java
@@ -27,12 +27,14 @@ public class HolaMundo {
 
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite su nombre: ");
-        var usuario2 = entrada.nextLine();//Variable tipo string (nextline lee una linea completa)
+        //Variable tipo string (nextline lee una linea completa)
+        var usuario2 = entrada.nextLine();
         System.out.println("usuario2 = " + usuario2);
         System.out.println("Escriba el titulo: ");
         var titulo2 = entrada.nextLine();
         System.out.println("titulo2 = " + titulo2 + " " + usuario2);
         */
+        /*
         Scanner scanner = new Scanner(System.in);
         //Escribe tu solucion aqui
         System.out.println("Proporciona el Titulo");
@@ -40,7 +42,24 @@ public class HolaMundo {
         System.out.println("Proporciona el autor");
         String autor = scanner.nextLine();
         System.out.println(titulo + " quien lo escribio fue: " + autor);
+        */
 
+        /*byte numEnteroByte =  127;
+        System.out.println("numEnteroByte = " + numEnteroByte);
+        //Las clases comienzan con la Letra Mayuscula (Byte)
+        System.out.println("Valor minimo del Byte: " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo del Byte: " + Byte.MAX_VALUE);*/
+
+
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("El valor minimo de float: " + Float.MIN_VALUE);
+        System.out.println("El valor maximo de float: " + Float.MAX_VALUE);
+
+        double numDouble = 1.7976931348623157E308;
+        System.out.println("numDoble = " + numDouble);
+        System.out.println("El valor minimo de double: " + Double.MIN_VALUE);
+        System.out.println("El valor maximo de double: " + Double.MAX_VALUE);
 
     }
 }
