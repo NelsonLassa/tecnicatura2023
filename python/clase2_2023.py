1d701cefce585cd2f957a1a0a028a90cade5f9ed
Lassa <nelsonlassa63@gmail.com>
2023-06-19 19:13:18 -0300

    Print e input final clase 3

--- python/clase2_2023.py
+++ python/clase2_2023.py
@@ -29,4 +29,40 @@ x = False
 print(x)
 print(type(x))
 
+# Manejo de cadenas (String)
+miGrupoFavorito = "La Konga "
+caracteristicas = "La mejor banda para bailar y sacar una sonrisa"
+print("Mi grupo favorito es: ", miGrupoFavorito, caracteristicas)
 
+numero1 = "7"
+numero2 = "8"
+print(int(numero1) + int(numero2))
+
+# Tipos Boleanos (bool)
+miBooleano = 3 > 2
+print(miBooleano)
+
+if miBooleano:
+    print("El resultado es verdadero")
+else:
+    print("El resultado es falso")
+
+# Procesar la entrada del usuario
+# funcion input
+resultado = input("Digite un numero: ")  # regresa un dato tipo string
+print(resultado)
+
+# Conversión de la entrada de datos
+numero1 = int(input("Escribe el primer numero: "))
+numero2 = int(input("Escribe el segundo numero: "))
+resultado = numero1 + numero2
+print("El resultado de la suma es: ", resultado)
+
+# Ejercicio 1
+dia = input("¿Como estuvo tu día? ")
+print("Mi día estuvo de: " + dia)
+
+# Ejercicio 2
+titulo = input("Proporciona el titulo: ")
+autor = input("Proporciona el autor: ")
+print(titulo, "fue escrito por",autor)
