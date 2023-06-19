9ab5e53812e8cff252a0fa58e22f74f49d175d5d
Lassa <nelsonlassa63@gmail.com>
2023-06-19 18:09:22 -0300

    tipos de datos clase 3

--- python/clase2_2023.py
+++ python/clase2_2023.py
@@ -11,3 +11,22 @@ print(id(x))
 # Las literales se escriben x328, la variable y = x072, la variable = x392
 print(id(y))
 print(id(z))
+
+# Tipos int, float, String, Bool
+x = 10
+print(x)
+print(type(x))
+x = 14.5
+print(x)
+print(type(x))
+x = "Hola alumnos"
+print(x)
+print(type(x))
+x = True
+print(x)
+print(type(x))
+x = False
+print(x)
+print(type(x))
+
+
